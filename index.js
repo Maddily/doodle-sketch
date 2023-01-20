6996d53e40d21f7379bd22df4d2cffb9d82f2654
Maddie <115709272+Maddily@users.noreply.github.com>
2023-01-21 01:40:08 +0200

    Append child divs to childFragment in the loop

--- index.js
+++ index.js
@@ -15,4 +15,5 @@ container.appendChild(mainFragment);
 for (let i = 1; i < 17; i++) {
     const div = document.createElement('div');
     div.classList.add('child-div');
+    childFragment.appendChild(div);
 }
