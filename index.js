e1d8f71e75f52479d4d9cc121859a9de3690540c
Maddie <115709272+Maddily@users.noreply.github.com>
2023-01-21 01:11:08 +0200

    Add a class to divs created inside the loop

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const fragment = document.createDocumentFragment();
 
 for (let i = 1; i < 17; i++) {
     const div = document.createElement('div');
-    
+    div.classList.add('horizontal-container');
 }
