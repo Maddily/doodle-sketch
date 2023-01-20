d0a86b225beb696756fb0c388c899f77a512115b
Maddie <115709272+Maddily@users.noreply.github.com>
2023-01-21 01:34:10 +0200

    Create a document fragment for child divs

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const container = document.querySelector('.container');
 const mainFragment = document.createDocumentFragment();
-
+const childFragment = document.createDocumentFragment();
 
 for (let i = 1; i < 17; i++) {
     const div = document.createElement('div');
     div.classList.add('horizontal-container');
-    mainFragment
-.appendChild(div);
+    mainFragment.appendChild(div);
 }
 
-container.appendChild(mainFragment
-);
+container.appendChild(mainFragment);
