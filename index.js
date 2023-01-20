372d5ca0ccc91ba9f66e2f541159962fa4816977
Maddie <115709272+Maddily@users.noreply.github.com>
2023-01-21 01:09:44 +0200

    Create a div inside the loop

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const container = document.querySelector('.container');
 const fragment = document.createDocumentFragment();
 
 for (let i = 1; i < 17; i++) {
+    const div = document.createElement('div');
     
 }
