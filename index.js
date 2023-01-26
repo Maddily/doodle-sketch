0fc8371664c2b41e8eea08b73627f7b5c9d8b5f0
Maddie <115709272+Maddily@users.noreply.github.com>
2023-01-26 15:26:16 +0200

    Change gridSize upon getting user input

--- index.js
+++ index.js
@@ -42,5 +42,8 @@ button.textContent = 'Change Grid Size';
 
 function changeGridSize() {
     const newGridSize = parseInt(prompt('How many squares per side?\n(Choose a number up to 100)', 0));
-    
+    if (newGridSize <= 100 && newGridSize >= 1) {
+        gridSize = newGridSize;
+            
+    }
 }
