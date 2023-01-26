5a54e10fb8368fe47ef378c9a3fd7caa5e8bd1bd
Maddie <115709272+Maddily@users.noreply.github.com>
2023-01-26 15:29:29 +0200

    Copy the process of building the grid into changeGridSize()

--- index.js
+++ index.js
@@ -45,5 +45,18 @@ function changeGridSize() {
     if (newGridSize <= 100 && newGridSize >= 1) {
         gridSize = newGridSize;
         container.innerHTML = '';
+        for (let i = 1; i <= gridSize; i++) {
+            const div = document.createElement('div');
+            div.classList.add('horizontal-container');
+            mainFragment.appendChild(div);
+        }
+        
+        container.appendChild(mainFragment);
+
+        horizontalContainers = document.querySelectorAll('.horizontal-container');
+        horizontalContainers.forEach(appendChildFragment);
+
+        childDiv = document.querySelectorAll('.child-div');
+        childDiv.forEach(listenForHover);
     }
 }
